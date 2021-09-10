d20d9bdb09f902d6c3763660e1670869e4e4b343
artemmoroz0v <90412503+artemmoroz0v@users.noreply.github.com>
2021-09-10 22:25:11 +0300

    Update figure.h

--- lab1/figure.h
+++ lab1/figure.h
@@ -6,7 +6,7 @@
 class Figure {
 public:
     virtual double Area() = 0;
-    virtual void Print(std::ostream &os) = 0;
+    virtual void Print(std::ostream &OutputStream) = 0;
     virtual size_t VerticesNumber() = 0; 
     virtual ~Figure() {};
 };
