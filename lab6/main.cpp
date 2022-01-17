3bbaed7ec1521a3f10908a0d6a4d5f73b3e7784a
Artem Morozov <90412503+artemmoroz0v@users.noreply.github.com>
2022-01-17 18:03:04 +0300

    Add files via upload

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -13,23 +13,18 @@ int main () {
     Pentagon c (std::cin);
     std:: cout << "The area of your figure is : " << c.Area() << std:: endl;
 
-    Pentagon d (std::cin);
-    std:: cout << "The area of your figure is : " << d.Area() << std:: endl;
-
-    Pentagon e (std::cin);
-    std:: cout << "The area of your figure is : " << e.Area() << std:: endl;
-
-    //lab2
+    //lab4
     TBinaryTree<Pentagon> tree;
     std:: cout << "Is tree empty? " << tree.Empty() << std:: endl;
-    tree.Push(a);
     std:: cout << "And now, is tree empty? " << tree.Empty() << std:: endl;
+    tree.Push(a);
     tree.Push(b);
     tree.Push(c);
-    tree.Push(d);
-    tree.Push(e);
     std:: cout << "The number of figures with area in [minArea, maxArea] is: " << tree.Count(0, 100000) << std:: endl;
     std:: cout << "The result of searching the same-figure-counter is: " << tree.root->ReturnCounter() << std:: endl;
     std:: cout << "The result of function named GetItemNotLess is: " << tree.GetItemNotLess(0, tree.root) << std:: endl;
+    std:: cout << tree << std:: endl;
+    tree.root = tree.Pop(tree.root, a);
+    std:: cout << tree << std:: endl;
     return 0;
 }
